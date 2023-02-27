2d1ad3375e22356ae3c4291ae162bd8304c7546f
Alumno ULSA <alumno@D513.local>
2023-02-27 14:34:07 -0700

    tabla alumnos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlumnosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alumnos', [AlumnosController::class, 'index'])
+->name('alumnos.index');
